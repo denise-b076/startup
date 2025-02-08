9638cada7ca62ce7dd75c9a0a7c9715a16199921
Denise Braithwaite <denisebraithw8@icloud.com>
2025-02-08 12:56:58 -0700

    added functions to .jsx files

---
+++ src/inspire/inspire.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Inspire() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>inspire displayed here</div>
+    </main>
+  );
+}
---
+++ src/palette_maker/palette_maker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Palette() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>palette maker displayed here</div>
+    </main>
+  );
+}
