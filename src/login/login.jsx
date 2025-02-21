1312d3c3b1ad46cbadea8f5479a52d71c8c1d9fd
Denise Braithwaite <denisebraithw8@icloud.com>
2025-02-20 21:06:12 -0700

    added placeholder for pic API in login

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 export function Login() {
+  const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
+
+  React.useEffect(() => {
+    setImageUrl(`placeholder.png`);
+  }, []);
+  
   return (
     <main className="container-fluid text-center">
-        <img className="picture_box" src="placeholder.png" alt="random_artwork" />
+        <div id='picture' className='picture-box'>
+          <img src={imageUrl} alt='random image' />
+        </div>
         <div className="login_form">
         <h2>Welcome to Tint-Hint!</h2>
         <form method="get">
