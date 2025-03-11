eb0bc040114affc610ea908fad3bc47ff0027038
Denise Braithwaite <denisebraithw8@icloud.com>
2025-03-11 17:17:32 -0600

    remove console.log for img id + title in login.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -9,13 +9,11 @@ export function Login({ userName, authState, onAuthChange, color, setColor }) {
 
   React.useEffect(() => {
     const random = Math.floor(Math.random() * 100);
-    console.log(random);
     fetch(`https://api.artic.edu/api/v1/artworks/search?query[term][is_public_domain]=true&limit=1&page=${random}&fields=id,title,image_id`)
       .then((response) => response.json())
       .then((responseData) => {
         const apiUrl = `${responseData.config.iiif_url}/${responseData.data[0].image_id}/full/843,/0/default.jpg`
         setImageUrl(apiUrl);
-        console.log(responseData.data[0].title)
       })
       .catch();
   }, []);
