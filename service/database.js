641a6fef7e645f3604f36fa9e26672299b4b62d1
Denise Braithwaite <denisebraithw8@icloud.com>
2025-03-12 21:08:02 -0600

    created user database functions

---
+++ service/database.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const userCollection = db.collection('users');
+const inspireCollection = db.collection('inspire');
+
+(async function testConnection() {
+    try {
+      await db.command({ ping: 1 });
+      console.log(`Connect to database`);
+    } catch (ex) {
+      console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+      process.exit(1);
+    }
+  })();
+
+function getUser(email) {
+    return userCollection.findOne({ email: email });
+
+}
+
+function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+}
+
+async function addUser(user) {
+    await userCollection.insertOne(user);
+}
+
+async function updateUser(user) {
+    await userCollection.updateOne({ email: user.email }, { $set: user });
+}
+
+module.exports = {
+    getUser,
+    getUserByToken,
+    addUser,
+    updateUser,
+}
