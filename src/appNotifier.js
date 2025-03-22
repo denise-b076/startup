ebe8981f26fe1a9d716f74b3370ee206a293f5c3
Denise Braithwaite <denisebraithw8@icloud.com>
2025-03-22 14:07:12 -0600

    add AppEventNotifier and exports in appNotifier.js

--- src/appNotifier.js
+++ src/appNotifier.js
@@ -10,3 +10,51 @@ class EventMessage {
         this.value = value;
     }
 }
+
+class AppEventNotifier {
+    events = [];
+    handlers = [];
+
+    constructor() {
+        let port = window.location.port;
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+        this.socket.onopen = (event) => {
+            this.receiveEvent(new EventMessage('Tint-Hint', AppEvent.System, { msg: 'connected' }));
+        };
+        this.socket.onclose = (event) => {
+            this.receiveEvent(new EventMessage('Tint-Hint', AppEvent.System, { msg: 'disconnected' }));
+        };
+        this.socket.onmessage = async (msg) => {
+            try {
+                const event = JSON.parse(await msg.data.text());
+                this.receiveEvent(event);
+            } catch {}
+        };
+    }
+
+    broadcastEvent(from, type, value) {
+        const event = new EventMessage(from, type, value);
+        this.socket.send(JSON.stringify(event));
+    }
+
+    addHandler(handler) {
+        this.handlers.push(handler);
+    }
+
+    removeHandler(handler) {
+        this.handlers.filter((h) => h !== handler);
+    }
+
+    receiveEvent(event) {
+        this.events.push(event);
+        this.events.forEach((e) => {
+            this.handlers.forEach((handler) => {
+                handler(e);
+            });
+        });
+    }
+}
+
+const AppNotifier = new AppEventNotifier();
+export { AppEvent, AppNotifier };
