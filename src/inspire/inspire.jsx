e359cc6b6ddcaa57aadb7f0a61e489baa26c7474
Denise Braithwaite <denisebraithw8@icloud.com>
2025-03-22 15:38:55 -0600

    deleted commented/unused code from inspire.jsx

--- src/inspire/inspire.jsx
+++ src/inspire/inspire.jsx
@@ -1,38 +1,16 @@
 import React from 'react';
 import "./inspire.css";
 import { useNavigate } from 'react-router-dom';
-import { randomizeColor, savePalette } from '../utilities';
 import { empty, tableMaker } from '../tableMaker';
 import { AppEvent, AppNotifier } from '../appNotifier';
 
-export function Inspire({color, setColor}) {
+export function Inspire({setColor}) {
     const [inspireData, setInspireData] = React.useState([]);
     const [inspireRows, setInspireRows] = React.useState([]);
     const [events, setEvent] = React.useState([]);
     const navigate = useNavigate();
 
     React.useEffect(() => {
-        // const intervalId = setInterval(() => {
-        //     setColor((color) => ({
-        //         ...color,
-        //         colorOne: randomizeColor(),
-        //         colorTwo: randomizeColor(),
-        //         colorThree: randomizeColor(),
-        //         colorFour: randomizeColor(),
-        //     }));
-
-        //     const names = ['tomBrady', 'chuckNorris', 'bruceWayne'];
-        //     const randomName = names[Math.floor(Math.random() * names.length)];
-        //     savePalette(`Palette ${randomName}`, color, 'inspire', randomName);
-        // }, 7000);
-
-        // fetch('/api/palettes/inspirePalettes')
-        // .then((response) => response.json())
-        // .then((palettes) => {
-        //     setInspireData(palettes);
-        // });       
-
-        // return () => clearInterval(intervalId);
         fetch('/api/palettes/inspirePalettes')
         .then((response) => response.json())
         .then((palettes) => {
@@ -44,9 +22,7 @@ export function Inspire({color, setColor}) {
         return () => {
             AppNotifier.removeHandler(handleAppEvent);
         };
-    }
-    // , [color]
-    );
+    });
 
     React.useEffect(() => {
         if (inspireData.length) {
